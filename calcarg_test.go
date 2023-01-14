96422a3c4b9accb713adb4515ecd0b6f52c49dce
dlwm <chinajb@foxmail.com>
2023-01-14 17:22:34 +0800

    支持小数运算

--- calcarg_test.go
+++ calcarg_test.go
@@ -8,18 +8,16 @@ import (
 
 func TestCalc(t *testing.T) {
 	start := time.Now().UnixMilli()
-	calculator, err := Analyse("(100-<age>)*<health>/100")
+	calculator, err := Analyse("(99-<age>)*<health>/88.88")
 	if err != nil {
 		panic(err)
 	}
-	for i := 0; i < 100000; i++ {
-		res, err := calculator.Eval(map[string]float32{
-			"age":    21,
-			"health": 60,
-		})
-		_, _ = res, err
-		//fmt.Println(res, err)
-	}
+	res, err := calculator.Eval(map[string]float32{
+		"age":    21,
+		"health": 60,
+	})
+	_, _ = res, err
+	fmt.Println(res, err)
 	end := time.Now().UnixMilli()
 	fmt.Println(end - start)
 }
